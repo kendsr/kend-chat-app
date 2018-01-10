1cfc34db203c416206a03c6af5bcefc7f7bb4bfc
kendsr <kndulin@comcast.net>
2018-01-10 11:34:05 -0600

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const express = require('express');
+
+const port = process.env.PORT || 3000;
+
+var app = express();
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.listen(port, () => {console.log(`Server up on port ${port}`)});
