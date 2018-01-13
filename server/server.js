e09dba2a631aab3ab6efeae967a4d47dba72b872
kendsr <kndulin@comcast.net>
2018-01-13 12:00:16 -0600

    Added broadccast

--- server/server.js
+++ server/server.js
@@ -14,6 +14,19 @@ app.use(express.static(path.join(__dirname, '../public')));
 io.on('connection', (socket) => {
     console.log("New user connected");
 
+    // socket.emit from Admin text Welcome to chat app
+    socket.emit('newMessage', {
+        From:'Admn', 
+        text:'Welcome to chat app',
+        createdAt: new Date().getTime()
+    });
+    // socket.broadcast.emit from Admin text New user joined
+    socket.broadcast.emit('newMessage', {
+        from:'Admin', 
+        text:'New User Joined',
+        createdAt: new Date().getTime()
+    });
+
     socket.on('createMessage', (message) => {
         console.log('createMsg:',message);
         io.emit('newMessage', {
@@ -21,6 +34,11 @@ io.on('connection', (socket) => {
             text: message.text,
             createdAt: new Date().getTime()
         });
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // });
     });
 
     socket.on('disconnect', () => {
