99273f1b3c39ef7740e737dd7483d2759f3d72e4
kendsr <kndulin@comcast.net>
2018-01-18 19:40:42 -0600

    Add message form and show messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -9,12 +9,17 @@ socket.on('disconnect', function() {
 });
 
 socket.on('newMessage', function(message){
-    console.log('received new message:', message);
+    var li = $('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+    $('#messages').append(li);
 });
 
-socket.emit('createMessage', {
-    from: 'Kend',
-    text: 'Hi'
-}, function(data){
-    console.log("Got it", data);
+$('#message-form').on('submit', function (e){
+    e.preventDefault();
+    socket.emit('createMessage', {
+        from: 'User',
+        text: $('[name=message]').val()
+    }, function (){
+
+    });
 });
