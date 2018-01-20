a73f709f2d47138acee582ea0d2b0a52f2dfcf32
kendsr <kndulin@comcast.net>
2018-01-20 11:48:31 -0600

    Add styles to page

--- public/js/index.js
+++ public/js/index.js
@@ -26,11 +26,14 @@ socket.on('newLocationMessage', function (message) {
 
 $('#message-form').on('submit', function (e){
     e.preventDefault();
+
+    var messageTextbox = $('[name=message]');
+
     socket.emit('createMessage', {
         from: 'User',
-        text: $('[name=message]').val()
+        text: messageTextbox.val()
     }, function (){
-
+        messageTextbox.val('');
     });
 });
 
@@ -40,12 +43,16 @@ locationButton.on('click', function () {
     return alert('Geolocation not supported by your browser.');
   }
 
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
+
   navigator.geolocation.getCurrentPosition(function (position) {
+      locationButton.removeAttr('disabled').text('Send Location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
   }, function () {
-    alert('Unable to fetch location.');
+    locationButton.removeAttr('disabled');
+    alert('Unable to fetch location.').text('Send Location');
   });
 });
